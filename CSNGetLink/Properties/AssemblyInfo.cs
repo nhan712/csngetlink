993cc2ee5a75fcaf9b9c974dbf530e0714e368bb
nhan712 <chinhan712@gmail.com>
2015-10-23 18:23:54 +0700

    update

--- CSNGetLink/Properties/AssemblyInfo.cs
+++ CSNGetLink/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSNDownloader")]
+[assembly: AssemblyTitle("CSNGetLink")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSNDownloader")]
+[assembly: AssemblyProduct("CSNGetLink")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
